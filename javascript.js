5595f3484aca55f0fcb098ab525e444eaad8ebbe
Trevor Situ <trevorsitu8@gmail.com>
2022-08-26 01:09:06 -0700

    results of basic operations should now display correctly after calcuation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,39 @@
+const container = document.querySelector("#container");
+// const numbers = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".ops");
+const equals = document.getElementById("equal");
+const clears = document.getElementById("clear");
+const display = document.querySelector(".display");
+
+let displayNum = "";
+let displayOps = "";
+
+document.querySelectorAll(".num").forEach(item => {
+    item.addEventListener('click', event => {
+
+        display.textContent += item.textContent;
+        
+        displayNum = display.textContent;
+        
+    })
+});
+
+clears.addEventListener("click", function() {
+
+    displayNum = "";
+    displayOps = "";
+    display.textContent = "";
+});
+
+document.querySelectorAll(".ops").forEach(item => {
+    item.addEventListener("click", event => {
+
+        display.textContent += (" " + item.textContent + " ");
+        displayOps = item.textContent;
+        displayNum = display.textContent;
+
+    })
+});
 
 function add (x, y) {
     return x + y;
